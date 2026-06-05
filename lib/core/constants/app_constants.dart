0722313ba3117b2b10a57b31a705be28554bc06b
Farhad Karimov <147695615+fourhadd@users.noreply.github.com>
2026-06-05 21:32:43 +0400

    Update API key to a placeholder
    Replaced the API key with a placeholder for security.

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,5 +4,5 @@ class AppConstants {
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
   static const String originalImageBaseUrl =
       'https://image.tmdb.org/t/p/original';
-  static const String apiKey = '4e4668e6e0023c9c125ad79ed34bc98a';
+  static const String apiKey = 'SƏNİN_TMDB_API_AÇARIN';
 }
